eaed9fe6004b1c375c2a3e065deb8fd6fa9dea62
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-10 22:37:19 -0800

    Update champions-model.js

--- api/champions/champions-model.js
+++ api/champions/champions-model.js
@@ -17,6 +17,56 @@ function findById(id) {
     .first()
 }
 
+function find() {
+    let items = db('items');
+
+    let newItemsArray = items.map(item => this.findById(item.id))
+
+    return newItemsArray
+}
+
+
+function findById(id) {
+    let items = db('items');
+
+    if (id) {
+        
+        items.where({ id }).first();
+
+        if(items.isBase) {
+            const promises = [items, this.getItemPossibilties(id)];
+
+            return Promise.all(promises).then(results => {
+                let [ item, builds ] = results;
+
+                if (item) {
+                    item.builds = builds;
+
+                    return item
+                } else {
+                    return null;
+                }
+            });
+        } else {
+            const promises = [items, this.getItemCombo(id)];
+
+            return Promise.all(promises).then(results => {
+                let [ item, combo ] = results;
+
+                if (item) {
+                    item.combo = combo;
+
+                    return item
+                } else {
+                    return null;
+                }
+            });
+        }
+    }
+
+    return items
+}
+
 function add(champion) {
     return db('champions')
         .insert(champion, '*')
