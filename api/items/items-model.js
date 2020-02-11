cdd260c3cd4f36a20a59aa4f2bb422eb7f9e124e
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-02-10 22:37:25 -0800

    Update items-model.js

--- api/items/items-model.js
+++ api/items/items-model.js
@@ -4,17 +4,62 @@ module.exports = {
     findById,
     add,
     update,
-    remove
+    remove,
+    getItemPossibilties,
+    getItemCombo
 }
 
 function find() {
-    return db('items');
+    let items = db('items');
+
+    let newItemsArray = items.map(item => {
+
+        return this.findById(item.id)
+    })
+
+    return newItemsArray
 }
 
+
 function findById(id) {
-    return db('items')
-    .where({ id })
-    .first()
+    let items = db('items');
+
+    if (id) {
+        
+        items.where({ id }).first();
+
+        if(items.isBase) {
+            const promises = [items, this.getItemPossibilties(id)];
+
+            return Promise.all(promises).then(results => {
+                let [ item, builds ] = results;
+
+                if (item) {
+                    item.builds = builds;
+
+                    return item
+                } else {
+                    return null;
+                }
+            });
+        } else {
+            const promises = [items, this.getItemCombo(id)];
+
+            return Promise.all(promises).then(results => {
+                let [ item, combo ] = results;
+
+                if (item) {
+                    item.combo = combo;
+
+                    return item
+                } else {
+                    return null;
+                }
+            });
+        }
+    }
+
+    return items
 }
 
 function add(item) {
@@ -32,4 +77,20 @@ function remove(id) {
     return db('items')
     .where({ id })
     .del();
+}
+
+
+function getItemPossibilties(id) {
+    let query = db('item_combos')
+        .where('base1_id', id)
+        .orWhere('base2_id', id)
+           
+    return query
+}
+
+function getItemCombo(id) {
+    let query = db('item_combos')
+        .where('item_id', id)
+           
+    return query
 }
